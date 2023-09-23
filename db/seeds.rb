b3b1ff67aa0d0dc2093e6000d6aed57570b64e4b
Leslie Torres <Leslietorresc@gmail.com>
2023-09-22 22:57:37 -0300

    commit elimine photo de seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-User.create!(email: "admin@gmail.com", name: "Esteban", password: "112233", role: 1, photo: File.open(Rails.root.join('app', 'assets', 'images', '1a.jpg'))) #asi es con la semillaaaaaaa
-User.create!(email: "usuarionormal@gmail.com", name: "Usuario1", password: "112233", role: 0,  photo: File.open(Rails.root.join('app', 'assets', 'images', '2u.jpg'))) #asi es con la semillaaaaaaa
+User.create!(email: "admin@gmail.com", name: "Esteban", password: "112233", role: 1) #asi es con la semillaaaaaaa
+User.create!(email: "usuarionormal@gmail.com", name: "Usuario1", password: "112233", role: 0) #asi es con la semillaaaaaaa
