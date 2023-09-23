9f37b25b826a6eb0e8c04309e6eae22e59c28128
Leslie Torres <Leslietorresc@gmail.com>
2023-09-22 22:50:13 -0300

    commit seed este si

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-User.create!(email: "admin@gmail.com", name: "Esteban", password: "112233", role: 1, photo: File.open(Rails.root.join('app', 'assets', 'images', 'user_photos', '1a.jpg'))) #asi es con la semillaaaaaaa
-User.create!(email: "usuarionormal@gmail.com", name: "Usuario1", password: "112233", role: 0,  photo: File.open(Rails.root.join('app', 'assets', 'images', 'user_photos', '2u.jpg'))) #asi es con la semillaaaaaaa
+User.create!(email: "admin@gmail.com", name: "Esteban", password: "112233", role: 1, photo: File.open(Rails.root.join('app', 'assets', 'images', '1a.jpg'))) #asi es con la semillaaaaaaa
+User.create!(email: "usuarionormal@gmail.com", name: "Usuario1", password: "112233", role: 0,  photo: File.open(Rails.root.join('app', 'assets', 'images', '2u.jpg'))) #asi es con la semillaaaaaaa
